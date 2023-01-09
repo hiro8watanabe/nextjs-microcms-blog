771e934de509f63536920fcf339bea08f13f7b4b
hiro <onecast.onelost615@gmail.com>
2023-01-09 19:31:02 +0900

    カテゴリーアーカイブページを追加

---
+++ src/pages/category.tsx
@@ -0,0 +1,96 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Layout } from "../components/Layout";
+import utilStyles from "../styles/Utils.module.css";
+import Link from "next/link";
+// import { getPostsData } from "lib/post";
+import { client } from "lib/client";
+import { formatDate } from "lib/util";
+
+//ssgの場合
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  // カテゴリーコンテンツの取得
+  const categoryData = await client.get({ endpoint: "categories" });
+  return {
+    props: {
+      blog: data.contents,
+      category: categoryData.contents,
+    },
+  };
+};
+
+// //ssrの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       //コンポーネントに渡すためのprops
+//     },
+//   };
+// }
+
+export default function Blog({ blog, category }) {
+  // console.log(category);
+  return (
+    <Layout blogs>
+      <Head>
+        <title>カテゴリーアーカイブ | PENGIN CODE</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width ,viewport-fit=cover"
+        />
+        <meta name="description" content="カテゴリーアーカイブページです" />
+      </Head>
+      <section
+        className={`${utilStyles.headingMd} ${utilStyles.smallContainer}`}
+      >
+        <p className={utilStyles.position}>
+          業界3年目のコーダーがフロントエンドの学習のため、Next.jsのSSGとMicroCMSを使って構築したJamstackブログです。
+        </p>
+      </section>
+
+      <div className={utilStyles.circle}></div>
+      <section
+        className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.position}`}
+      >
+        <h2>カテゴリーアーカイブ</h2>
+
+        <ul>
+          {category.map((category) => (
+            <li key={category.id}>
+              <Link href={`/category/${category.id}`}>{category.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.grid}>
+          {blog.map((blog) => (
+            <article key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                <Image
+                  src={blog.thumbnail.url}
+                  className={styles.thumbnailImage}
+                  width={950}
+                  height={534}
+                  objectFit="cover"
+                  alt={blog.title}
+                />
+              </Link>
+              <Link href={`/blog/${blog.id}`}>
+                <a
+                  className={`${utilStyles.boldText} ${utilStyles.inlineBlock}`}
+                >
+                  {blog.title}
+                </a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                {formatDate(blog.publishedAt)}
+              </small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
