0af491dbbc3bf5f2f4b8286a6156182ff11dd256
hiro <onecast.onelost615@gmail.com>
2023-01-10 17:13:49 +0900

    h1titleにリンク追加

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import headerStyles from "src/components/Header/Header.module.css";
+import Link from "next/link";
 
 const h1Title: string = "PENGIN CODE";
 
 export const Logo = () => {
   return (
-    <div className={headerStyles.logoWrapper}>
-      <Image
-        src="/images/pengin-icon.png"
-        alt="PENGIN CODE LOGO"
-        width={52}
-        height={52}
-        className={`${headerStyles.logo} `}
-      />
-      <h1 className={headerStyles.heading2Xl}>{h1Title}</h1>
-    </div>
+    <Link href="/">
+      <a>
+        <div className={headerStyles.logoWrapper}>
+          <Image
+            src="/images/pengin-icon.png"
+            alt="PENGIN CODE LOGO"
+            width={52}
+            height={52}
+            className={`${headerStyles.logo} `}
+          />
+          <h1 className={headerStyles.heading2Xl}>{h1Title}</h1>
+        </div>
+      </a>
+    </Link>
   );
 };
