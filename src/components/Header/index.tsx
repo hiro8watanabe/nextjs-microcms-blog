0fad35374d860973ef7e2ba205ff2db793b11ef5
hiro <onecast.onelost615@gmail.com>
2023-01-10 16:48:04 +0900

    Logoをcomponentへ切り出し

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import headerStyles from "src/components/Header/Header.module.css";
-import Image from "next/image";
-
-const h1Title: string = "PENGIN CODE";
+import { Logo } from "src/components/Logo";
 
 export function Header() {
   return (
     <header className={headerStyles.header}>
       <div className={headerStyles.inner}>
-        <div className={headerStyles.logoWrapper}>
-          <Image
-            src="/images/pengin-icon.png"
-            alt="PENGIN CODE LOGO"
-            width={52}
-            height={52}
-            className={`${headerStyles.logo} `}
-          />
-          <h1 className={headerStyles.heading2Xl}>{h1Title}</h1>
-        </div>
+        <Logo />
         <nav className={headerStyles.nav}>
           <ul className={headerStyles.lists}>
             <li className={headerStyles.list}>
