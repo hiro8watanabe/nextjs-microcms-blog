097da9d0e1cf22aca618c136eb4b592ab4611fc7
hiro <onecast.onelost615@gmail.com>
2023-01-17 00:38:54 +0900

    フォームバリデーション設定

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,49 +1,65 @@
 import React, { useState } from "react";
 import type { NextPage } from "next";
 import formStyles from "src/components/Form/Form.module.css";
-import Router from "next/router";
 import { Loading } from "src/components/Form/Loading";
 import { Success } from "src/components/Form/Success";
-
-type Props = {};
 
 export const Form: NextPage = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
-  // const [resData, setResData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState<boolean>(true);
   const [form, setForm] = useState({
     name: "",
     mail: "",
     msg: "",
   });
+  const [nameFlag, setNameFlag] = useState<boolean>(true);
+  const [mailFlag, setMailFlag] = useState<boolean>(true);
+  const [messageFlag, setMessageFlag] = useState<boolean>(true);
 
   const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setIsLoading(true);
+    // e.preventDefault();
 
-    fetch("/api/mail", {
-      method: "POST",
-      body: JSON.stringify({
-        name: form.name,
-        mail: form.mail,
-        msg: form.msg,
-      }),
-    })
-      .then((res) => {
-        console.log("Response received");
-        if (res.status === 200) {
-          // Router.push("/about");
-          setIsSuccess(true);
-          console.log("Response succeeded!");
-          setIsLoading(false);
-        } else {
-          console.log(`Error: Status Code ${res.status}`);
-        }
+    const name = form.name;
+    const mail = form.mail;
+    const msg = form.msg;
+
+    if (name === "" || mail === "" || msg === "") {
+      return;
+    } else {
+      setIsLoading(true);
+
+      fetch("/api/mail", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          mail: mail,
+          msg: msg,
+        }),
       })
-      .catch((e) => {
-        console.log(`Error: ${e}`);
-        setIsLoading(false);
-      });
+        .then((res) => {
+          console.log("Response received");
+          if (res.status === 200) {
+            setIsSuccess(true);
+            console.log("Response succeeded!");
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            setNameFlag(true);
+            setMailFlag(true);
+            setMessageFlag(true);
+            setErrorMessage(false);
+            console.log(`Error: Status Code ${res.status}`);
+          }
+        })
+        .catch((e) => {
+          console.log(`Error: ${e}`);
+          setNameFlag(true);
+          setMailFlag(true);
+          setMessageFlag(true);
+          setIsLoading(false);
+        });
+    }
   };
 
   return (
@@ -60,13 +76,13 @@ export const Form: NextPage = () => {
             </label>
             <input
               onChange={(e) => {
-                const val = e.currentTarget.value;
+                const val = e.target.value;
                 setForm((props) => ({
                   ...props,
                   name: val !== null ? val : "",
                 }));
+                setNameFlag(val !== "" ? false : true);
               }}
-              value={form.name}
               id="name"
               name="name"
               type="text"
@@ -84,11 +100,12 @@ export const Form: NextPage = () => {
             </label>
             <input
               onChange={(e) => {
-                const val = e.currentTarget.value;
+                const val = e.target.value;
                 setForm((props) => ({
                   ...props,
                   mail: val !== null ? val : "",
                 }));
+                setMailFlag(val !== "" ? false : true);
               }}
               id="mail"
               name="mail"
@@ -107,11 +124,12 @@ export const Form: NextPage = () => {
             </label>
             <textarea
               onChange={(e) => {
-                const val = e.currentTarget.value;
+                const val = e.target.value;
                 setForm((props) => ({
                   ...props,
                   msg: val !== null ? val : "",
                 }));
+                setMessageFlag(val !== "" ? false : true);
               }}
               id="text"
               name="text"
@@ -120,11 +138,16 @@ export const Form: NextPage = () => {
               required
             ></textarea>
           </div>
+          {errorMessage ? undefined : <p className={formStyles.error}>入力内容に誤りがあります。</p>}
           <button
             onClick={(e: any) => {
               handleSubmit(e);
             }}
-            className={formStyles.submitBtn}
+            className={
+              nameFlag || mailFlag || messageFlag
+                ? `${formStyles.submitBtnDisabled}`
+                : `${formStyles.submitBtnDisabled} ${formStyles.submitBtnActive}`
+            }
             type="submit"
           >
             SUBMIT
