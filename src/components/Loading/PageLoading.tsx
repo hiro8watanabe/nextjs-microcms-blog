89754da24d37c11dd24bbe49d61c0d957cd03705
hiro <onecast.onelost615@gmail.com>
2023-01-18 20:40:48 +0900

    初回ローディング実装

---
+++ src/components/Loading/PageLoading.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./PageLoading.module.css";
+
+export const PageLoading = () => {
+  return (
+    <div className={styles.mask}>
+      <div className={styles.wrap}>
+        <div className={styles.container}>
+          <p className={styles.circleText}>
+            <span>R</span>
+            <span>e</span>
+            <span>a</span>
+            <span>c</span>
+            <span>t</span>
+            <span></span>
+            <span>♥</span>
+            <span></span>
+            <span>N</span>
+            <span>e</span>
+            <span>x</span>
+            <span>t</span>
+            <span>.j</span>
+            <span>s</span>
+            <span></span>
+            <span>♣</span>
+            <span></span>
+            <span>T</span>
+            <span>y</span>
+            <span>p</span>
+            <span>e</span>
+            <span>S</span>
+            <span>c</span>
+            <span>r</span>
+            <span>i</span>
+            <span>p</span>
+            <span>t</span>
+            <span></span>
+            <span>♦</span>
+            <span></span>
+            <span>M</span>
+            <span>i</span>
+            <span>c</span>
+            <span>r</span>
+            <span>o</span>
+            <span>C</span>
+            <span>M</span>
+            <span>S</span>
+            <span></span>
+            <span>♠</span>
+            <span></span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import utilStyles from "src/styles/Utils.module.css";
 import Link from "next/link";
 import { client } from "lib/client";
 import { formatDate } from "lib/util";
+import { PageLoading } from "src/components/Loading/PageLoading";
+import { useCallback, useEffect, useState } from "react";
 
 //ssgの場合
 export const getStaticProps = async () => {
@@ -17,78 +19,98 @@ export const getStaticProps = async () => {
   };
 };
 
-// //ssrの場合
-// export async function getServerSideProps(context) {
+//ssrの場合
+// export const getServerSideProps = async () => {
+//   const data = await client.get({ endpoint: "blog" });
 //   return {
 //     props: {
-//       //コンポーネントに渡すためのprops
+//       blog: data.contents,
 //     },
 //   };
-// }
+// };
 
 export default function Home({ blog }) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    !sessionStorage.getItem("visited")
+      ? setTimeout(() => {
+          setLoading(false);
+          sessionStorage.setItem("visited", "first");
+        }, 3000)
+      : setLoading(false);
+  }, []);
+
   return (
-    <Layout home>
-      <Head>
-        <title>PENGIN CODE | Next.jsとMicroCMSを使用したJamstackブログ</title>
-        {/* <title>{siteTitle}</title> */}
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width, viewport-fit=cover"
-        />
-        <meta
-          name="description"
-          content="業界4年目のコーダーがフロントエンドの学習のため、Next.jsのSGとMicroCMSを使って構築したJamstackブログ"
-        />
-        <link
-          rel="canonical"
-          href="https://nextjs-microcms-blog-olive.vercel.app/"
-        />
-      </Head>
-      <section
-        className={`${utilStyles.headingMd} ${utilStyles.smallContainer}`}
-      >
-        <p className={utilStyles.position}>
-          業界4年目のコーダーがフロントエンドの学習のため、Next.jsのSGとMicroCMSを使って構築したJamstackブログ。仕事の合間にのんびり機能追加しながら、遭遇したエラーなどの対処法をひっそりと書いています。
-        </p>
-      </section>
+    <>
+      {loading ? (
+        <PageLoading />
+      ) : (
+        <Layout home>
+          <Head>
+            <title>
+              PENGIN CODE | Next.jsとMicroCMSを使用したJamstackブログ
+            </title>
+            {/* <title>{siteTitle}</title> */}
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+            />
+            <meta
+              name="description"
+              content="業界4年目のコーダーがフロントエンドの学習のため、Next.jsのSGとMicroCMSを使って構築したJamstackブログ"
+            />
+            <link
+              rel="canonical"
+              href="https://nextjs-microcms-blog-olive.vercel.app/"
+            />
+          </Head>
+          <section
+            className={`${utilStyles.headingMd} ${utilStyles.smallContainer}`}
+          >
+            <p className={utilStyles.position}>
+              業界4年目のコーダーがフロントエンドの学習のため、Next.jsのSGとMicroCMSを使って構築したJamstackブログ。仕事の合間にのんびり機能追加しながら、遭遇したエラーなどの対処法をひっそりと書いています。
+            </p>
+          </section>
 
-      <div className={utilStyles.circle}></div>
-      <section
-        className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.position}`}
-      >
-        <h2>新着記事一覧</h2>
-        <div className={styles.grid}>
-          {blog.map((blog) => (
-            <article key={blog.id}>
-              <Link href={`/blog/${blog.id}`}>
-                <a
-                  className={`${styles.moveImg} ${utilStyles.boldText} ${utilStyles.inlineBlock}`}
-                >
-                  <Image
-                    src={blog.thumbnail.url}
-                    className={styles.thumbnailImage}
-                    width={950}
-                    height={534}
-                    priority={true}
-                    objectFit="cover"
-                    placeholder="blur"
-                    blurDataURL={blog.thumbnail.url}
-                    alt={blog.title}
-                  />
+          <div className={utilStyles.circle}></div>
+          <section
+            className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.position}`}
+          >
+            <h2>新着記事一覧</h2>
+            <div className={styles.grid}>
+              {blog.map((blog) => (
+                <article key={blog.id}>
+                  <Link href={`/blog/${blog.id}`}>
+                    <a
+                      className={`${styles.moveImg} ${utilStyles.boldText} ${utilStyles.inlineBlock}`}
+                    >
+                      <Image
+                        src={blog.thumbnail.url}
+                        className={styles.thumbnailImage}
+                        width={950}
+                        height={534}
+                        priority={true}
+                        objectFit="cover"
+                        placeholder="blur"
+                        blurDataURL={blog.thumbnail.url}
+                        alt={blog.title}
+                      />
 
-                  {blog.title}
+                      {blog.title}
 
-                  <br />
-                  <small className={utilStyles.lightText}>
-                    {formatDate(blog.publishedAt)}
-                  </small>
-                </a>
-              </Link>
-            </article>
-          ))}
-        </div>
-      </section>
-    </Layout>
+                      <br />
+                      <small className={utilStyles.lightText}>
+                        {formatDate(blog.publishedAt)}
+                      </small>
+                    </a>
+                  </Link>
+                </article>
+              ))}
+            </div>
+          </section>
+        </Layout>
+      )}
+    </>
   );
 }
