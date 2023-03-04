856c476711e6fd867483fca7fa0ea94aca2c1aac
hiro <onecast.onelost615@gmail.com>
2023-03-04 16:21:26 +0900

    型追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 // import 'src/styles/Reset.css'
 import React from "react";
+import { AppProps } from "next/app";
 import "src/styles/Globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
       <Component {...pageProps} />
