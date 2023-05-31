80038005644373b04d71760295b9a04939ee7361
hiro <onecast.onelost615@gmail.com>
2023-05-31 10:20:32 +0700

    noindex設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ const Document = () => {
   return (
     <Html lang="ja">
       <Head>
-        <meta name="robots" content="index, follow" />
+        <meta name="robots" content="noindex, nofollow"/>
         <meta name="format-detection" content="telephone=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
